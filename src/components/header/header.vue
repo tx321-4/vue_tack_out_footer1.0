0540f683775f4a1720336b49853fe620b2514bbb
shi_yi <shi_yidashi@163.com>
2019-07-09 18:10:35 +0800

    add head 弹窗

--- src/components/header/header.vue
+++ src/components/header/header.vue
@@ -15,18 +15,19 @@
         <span class="text">{{seller.supports[0].description}}</span>
       </div>
     </div>
-    <div v-if="seller.supports" class="support-count">
+    <div v-if="seller.supports" class="support-count" @click="showDetail">
       <span class="count">{{seller.supports.length}}个</span>
       <i class="icon-keyboard_arrow_right"></i>
     </div>
   </div>
-  <div class="bulletin-wrapper">
+  <div class="bulletin-wrapper" @click="showDetail">
     <span class="bulletin-title"></span><span class="bulletin-text">{{seller.bulletin}}</span>
     <i class="icon-keyboard_arrow_right"></i>
   </div>
   <div class="background">
     <img :src="seller.avatar" width="100%" height="100%" alt="" />
   </div>
+  <div v-show="detailShow" class="detail"></div>
 </div>
 
 </template>
@@ -36,6 +37,16 @@ export default{
   props: {
     seller: {
       type: Object
+    }
+  },
+  data() {
+    return {
+      detailShow: false
+    };
+  },
+  methods: {
+    showDetail() {
+      this.detailShow = true;
     }
   },
   created() {
@@ -161,5 +172,13 @@ export default{
       height: 100%
       z-index: -1
       filter: blur(10px)
-
+    .detail
+      position: fixed
+      z-index: 100
+      top: 0
+      left: 0
+      width: 100%
+      height: 100%
+      overflow: auto
+      background: rgba(7, 17, 27, 0.8)
 </style>
