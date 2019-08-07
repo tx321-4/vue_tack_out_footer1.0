5040014e20039eb95303c786434edd38cb0bc235
shi_yi <shi_yidashi@163.com>
2019-08-07 18:19:06 +0800

    提交第一版

--- src/App.vue
+++ src/App.vue
@@ -12,28 +12,34 @@
          <router-link to="/seller">商家</router-link>
       </div>
     </div>
-    <router-view :seller="seller"></router-view>
+    <keep-alive>
+      <router-view :seller="seller"></router-view>
+    </keep-alive>
   </div>
 </template>
 
 <script type="text/ecmascript-6">
 import header from './components/header/header.vue';
-
+import {urlParse} from './common/js/util';
 const ERR_OK = 0;
 
 export default {
   data() {
     return {
       seller: {
-
+        id: (() => {
+        let queryParam = urlParse();
+        console.log(queryParam);
+        return queryParam.id;
+        })()
       }
     };
   },
   created() {
-    this.$http.get('/api/seller').then((response) => {
+    this.$http.get('/api/seller?id=' + this.seller.id).then((response) => {
       response = response.body;
       if (response.errno === ERR_OK) {
-        this.seller = response.data;
+        this.seller = Object.assign({}, this.seller, response.data);
         console.log(this.seller);
       }
     });
